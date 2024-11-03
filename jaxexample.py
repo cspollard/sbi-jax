031be8f7bd2f5f8e785d4857c56e6511dc7a3dc3
Chris Pollard <cspollard@gmail.com>
2024-11-03 10:09:20 +0000

    hm

--- jaxexample.py
+++ jaxexample.py
@@ -19,8 +19,8 @@ LR = 1e-3
 FINETUNELR = 1e-4
 NPLOTPOINTS = 2000
 
-NMAXSTART = 256
-NMAXFINETUNE = NMAXSTART
+NMAXSTART = 8
+NMAXFINETUNE = 256
 
 
 knext = random.PRNGKey(0)
@@ -73,37 +73,91 @@ for iepoch in range(NEPOCHS):
   plot(knext, NPLOTPOINTS, phi, rho, modelparams, NMAXFINETUNE, prefix="training/", label=f"_epoch{iepoch:02d}", ntrain=NMAXSTART, groundtruth=groundtruth)
 
 
-save_args = orbax_utils.save_args_from_target(modelparams)
+  save_args = orbax_utils.save_args_from_target(modelparams)
 
-orbax_checkpointer.save("/Users/cspollard/Physics/sbi-jax/nofinetune.orbax", modelparams, save_args=save_args, force=True)
-
-
-# sched = optax.cosine_decay_schedule(FINETUNELR , NEPOCHS*NBATCHES)
-# optimizer = optax.adam(learning_rate=sched)
-# opt_state = optimizer.init(modelparams)
-
-# for iepoch in range(NEPOCHS):
-#   print("epoch:", iepoch)
-#   for _ in tqdm(range(NBATCHES)):
-#     k, knext = splitkey(knext)
-#     labels = prior(k, BATCHSIZE)
-#     k, knext = splitkey(knext)
-#     batch , ns = gen(k, labels, NMAXFINETUNE)
-#     modelparams, opt_state, loss_value = \
-#       steprho(modelparams, opt_state, batch, ns, labels)
+  orbax_checkpointer.save("/Users/cspollard/Physics/sbi-jax/initial.orbax", modelparams, save_args=save_args, force=True)
 
 
-#   k, knext = splitkey(knext)
-#   plot(knext, NPLOTPOINTS, phi, rho, modelparams, NMAXFINETUNE, prefix="finetune/", label=f"_epoch{iepoch:02d}", ntrain=NMAXSTART, groundtruth=groundtruth)
+sched = optax.cosine_decay_schedule(FINETUNELR , NEPOCHS*NBATCHES)
+optimizer = optax.adam(learning_rate=sched)
+opt_state = optimizer.init(rhoparams)
+
+for iepoch in range(NEPOCHS):
+  print("epoch:", iepoch)
+  for _ in tqdm(range(NBATCHES)):
+    k, knext = splitkey(knext)
+    labels = prior(k, BATCHSIZE)
+    k, knext = splitkey(knext)
+    batch , ns = gen(k, labels, NMAXFINETUNE)
+    rhoparams, opt_state, loss_value = \
+      steprho(rhoparams, opt_state, batch, ns, labels)
 
 
-# state = \
-#   train_state.TrainState.create \
-#   ( apply_fn=fwd
-#   , params=modelparams
-#   , tx=optimizer
-#   )
+  k, knext = splitkey(knext)
+  plot \
+    ( knext
+    , NPLOTPOINTS
+    , phi
+    , rho
+    , { "phi" : modelparams["phi"] , "rho" : rhoparams }
+    , NMAXFINETUNE
+    , prefix="finetuned/"
+    , label=f"_epoch{iepoch:02d}"
+    , ntrain=NMAXSTART
+    , groundtruth=groundtruth
+    )
 
-# save_args = orbax_utils.save_args_from_target(modelparams)
+  save_args = orbax_utils.save_args_from_target(modelparams)
 
-# orbax_checkpointer.save("/Users/cspollard/Physics/sbi-jax/finetuned.orbax", modelparams, save_args=save_args, force=True)
+  orbax_checkpointer.save \
+    ( "/Users/cspollard/Physics/sbi-jax/finetuned.orbax"
+    , { "phi" : modelparams["phi"] , "rho" : rhoparams }
+    , save_args=save_args
+    , force=True
+    )
+
+
+k, knext = splitkey(knext)
+phiparams = phi.init(k, np.zeros((1, 13, 1)))
+k, knext = splitkey(knext)
+rhoparams = rho.init(k, phi.apply(phiparams, np.zeros((1, 1, 1))))
+
+modelparams = { "rho" : rhoparams , "phi" : phiparams }
+
+sched = optax.cosine_decay_schedule(LR , NEPOCHS*NBATCHES)
+optimizer = optax.adam(learning_rate=sched)
+opt_state = optimizer.init(modelparams)
+
+for iepoch in range(NEPOCHS):
+  print("epoch:", iepoch)
+  for _ in tqdm(range(NBATCHES)):
+    k, knext = splitkey(knext)
+    labels = prior(k, BATCHSIZE)
+    k, knext = splitkey(knext)
+    batch , ns = gen(k, labels, NMAXFINETUNE)
+    modelparams, opt_state, loss_value = \
+      steprho(modelparams, opt_state, batch, ns, labels)
+
+
+  k, knext = splitkey(knext)
+  plot \
+    ( knext
+    , NPLOTPOINTS
+    , phi
+    , rho
+    , modelparams
+    , NMAXFINETUNE
+    , prefix="finetuned/"
+    , label=f"_epoch{iepoch:02d}"
+    , ntrain=NMAXSTART
+    , groundtruth=groundtruth
+    )
+
+  save_args = orbax_utils.save_args_from_target(modelparams)
+
+  orbax_checkpointer.save \
+    ( "/Users/cspollard/Physics/sbi-jax/direct.orbax"
+    , modelparams
+    , save_args=save_args
+    , force=True
+    )
